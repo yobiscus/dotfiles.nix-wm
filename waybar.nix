bc295556fa688fac4888a7cc55a525f363323d4b
Jonathan Gravel <jo@stashed.dev>
2025-12-17 23:02:57 -0500

    waybar: customize

--- waybar.nix
+++ waybar.nix
@@ -1,9 +1,16 @@
 { config, pkgs, ... }:
 
 {
+  home.packages = [
+    pkgs.fira-sans
+    pkgs.font-awesome
+    pkgs.pavucontrol
+  ];
+
   programs.waybar = {
     enable = true;
   };
 
-  home.file.".config/waybar".source = ./config/waybar;
+  # home.file.".config/waybar".source = ./config/waybar;
+  home.file.".config/waybar".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/home-manager/modules/wm/config/waybar";
 }
