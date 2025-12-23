44c14ad02d210fa748b8da2c748e0d37fda0f091
Jonathan Gravel <jo@stashed.dev>
2025-12-23 13:18:31 -0500

    kitty: use matugen colors

--- kitty.nix
+++ kitty.nix
@@ -21,6 +21,12 @@
       selection_foreground = "none";
       selection_background = "none";
       cursor_trail = 1;
+      include = "colors-matugen.conf";
     };
   };
+
+  # colorsmatugen.conf is modified by Mutagen, so it has to be writable
+  home.file.".config/kitty/colors-matugen.conf".source =
+    config.lib.file.mkOutOfStoreSymlink
+    "${config.home.homeDirectory}/.dotfiles/config/home-manager/modules/wm/config/kitty/colors-matugen.conf";
 }
