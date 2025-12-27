32abb05055eb2982dbc5475b31bf708ecc791632
Jonathan Gravel <jo@stashed.dev>
2025-12-26 23:01:17 -0500

    matugen: create writable config to promote experimentation

--- matugen.nix
+++ matugen.nix
@@ -5,5 +5,8 @@
     pkgs.matugen
   ];
 
-  home.file.".config/matugen".source = ./config/matugen;
+  # home.file.".config/matugen".source = ./config/matugen;
+  home.file.".config/matugen".source =
+    config.lib.file.mkOutOfStoreSymlink
+    "${config.home.homeDirectory}/.dotfiles/config/home-manager/modules/wm/config/matugen";
 }
