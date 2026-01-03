3e2583c6bca9a4f9f932aa8e5ee8b717f4dbba87
Jonathan Gravel <jo@stashed.dev>
2026-01-03 09:30:33 -0500

    hyprland: manage hypridle in hyprland/waybar instead of systemd

--- hyprland.nix
+++ hyprland.nix
@@ -4,6 +4,7 @@
   home.packages = [
     pkgs.blueman
     pkgs.brightnessctl
+    pkgs.hypridle
     pkgs.hyprshot
     pkgs.wofi
   ];
@@ -101,46 +102,12 @@
     };
   };
 
-  services.hypridle = {
-      enable = true;
-      settings = {
-          general = {
-              lock_cmd = "pidof hyprlock || hyprlock";       # avoid starting multiple hyprlock instances.
-              before_sleep_cmd = "loginctl lock-session";    # lock before suspend.
-              after_sleep_cmd = "hyprctl dispatch dpms on";  # to avoid having to press a key twice to turn on the display.
-          };
-          listener = [
-              {
-                  timeout = 150;                             # 2.5min.
-                  on-timeout = "brightnessctl -s set 10";    # set monitor backlight to minimum, avoid 0 on OLED monitor.
-                  on-resume = "brightnessctl -r";            # monitor backlight restore.
-              }
-              {
-                  timeout = 150;                                              # 2.5min.
-                  on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0";   # turn off keyboard backlight.
-                  on-resume = "brightnessctl -rd rgb:kbd_backlight";          # turn on keyboard backlight.
-              }
-              {
-                  timeout = 300;                             # 5min
-                  on-timeout = "loginctl lock-session";      # lock screen when timeout has passed
-              }
-              {
-                  timeout = 330;                                              # 5.5min
-                  on-timeout = "hyprctl dispatch dpms off";                   # screen off when timeout has passed
-                  on-resume = "hyprctl dispatch dpms on && brightnessctl -r"; # screen on when activity is detected after timeout has fired.
-              }
-              {
-                  timeout = 1800;                            # 30min
-                  on-timeout = "systemctl suspend";          # suspend pc
-              }
-          ];
-      };
-  };
-
   home.file.".config/hypr/conf".source = ./config/hypr/conf;
   home.file.".config/hypr/scripts".source = ./config/hypr/scripts;
 
   home.file.".config/hypr/colors-mutagen.conf".source =
     config.lib.file.mkOutOfStoreSymlink
     "${config.home.homeDirectory}/.dotfiles/config/home-manager/modules/wm/config/hypr/colors-matugen.conf";
+
+  home.file.".config/hypr/hypridle.conf".source = ./config/hypr/hypridle.conf;
 }
