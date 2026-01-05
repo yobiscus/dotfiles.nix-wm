58a8499102feb280ec93743d47df93b8c63fb0d3
Jonathan Gravel <jo@stashed.dev>
2026-01-05 11:58:51 -0500

    kitty: use text config instead of home-manager config

--- kitty.nix
+++ kitty.nix
@@ -1,30 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  programs.kitty = {
-    enable = true;
-    settings = {
-      shell = "${pkgs.zsh}/bin/zsh";
-      font_family = "JetBrainsMono Nerd Font";
-      font_size = 10.5;
-      bold_font = "auto";
-      italic_font = "auto";
-      bold_italic_font = "auto";
-      cursor_blink_interval = 0.5;
-      cursor_stop_blinking_after = 1;
-      scrollback_lines = 9999;
-      enable_audio_bell = "no";
-      window_padding_width = 8;
-      hide_window_decorations = "yes";
-      background_opacity = 0.7;
-      dynamic_background_opacity = "yes";
-      selection_foreground = "none";
-      selection_background = "none";
-      cursor_trail = 1;
-      include = "colors-matugen.conf";
-    };
-  };
+  home.packages = [
+    pkgs.kitty
+  ];
 
+  home.file.".config/kitty/kitty.conf".source = ./config/kitty/kitty.conf;
   # colorsmatugen.conf is modified by Mutagen, so it has to be writable
   home.file.".config/kitty/colors-matugen.conf".source =
     config.lib.file.mkOutOfStoreSymlink
